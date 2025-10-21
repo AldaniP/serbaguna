26ebb4899569435e0f13b7d3ca3f570974e6994c
AldaniP <aldaniprasetyo@gmail.com>
2025-10-21 21:04:46 +0700

    chore: adjust build configuration and fix compatibility for Compressor and Downloader tools

---
+++ src/app/tools/compressor/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React from "react";
+
+export default function Compressor() {
+  return (
+    <div>
+      <h1>Compressor</h1>
+      <p>Tool untuk mengompres file secara online.</p>
+    </div>
+  );
+}
---
+++ src/app/tools/downloader/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import React from "react";
+
+export default function downloader() {
+  return (
+    <div>
+      <h1>donloader</h1>
+      <p>Tool untuk mengunduh file</p>
+    </div>
+  );
+}
