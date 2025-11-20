eb09c98a5f56be5ba6e369ff0c28d6922749854f
AldaniP <aldaniprasetyo@gmail.com>
2025-11-20 10:24:26 +0700

    feat: Add Babel configuration and React import to test file.

--- __tests__/home.test.tsx
+++ __tests__/home.test.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 // If the actual Home component exists elsewhere, update the path accordingly, e.g.:
